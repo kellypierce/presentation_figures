766918680eddf130af0a89673fc076e357fec831
Kelly Anne Pierce <kellyannepierce@gmail.com>
2025-07-06 18:56:09 -0500

    make overlap_buffer fxn of line count

--- presentation_figures/ecology_timeline.py
+++ presentation_figures/ecology_timeline.py
@@ -31,20 +31,21 @@ def round_nearest(value: float|int, nearest: int, direction: str) -> float:
         raise ValueError("Direction must be 'down' or 'up'")
 
 
-def make_data_century(data: pd.DataFrame) -> pd.DataFrame:
+def aggregate_data(data: pd.DataFrame, years: int) -> pd.DataFrame:
     """Round event years down to nearest century and group events by their century of occurrence."""
-    data['plot_date'] = [round_nearest(i) for i in data['relative_year']]
+    data['plot_date'] = [round_nearest(i, nearest=years, direction="down") for i in data['relative_year']]
     plot_data = data.groupby('plot_date')['event'].agg(lambda x: '\n'.join(x)).reset_index()
     return plot_data
 
 
-def calculate_text_height(data: pd.DataFrame, years_per_line=100) -> pd.DataFrame:
+def calculate_text_height(data: pd.DataFrame, years_per_line) -> pd.DataFrame:
 
     # count number of text lines
-    data['text_height'] = [len(re.findall("\n", i)) * years_per_line for i in data['event']]
+    data['n_lines'] = [len(re.findall("\n", i)) for i in data['event']]
+    data['text_height'] = data['n_lines'] * years_per_line
 
     # make all values positive and calculate bottom and top of text block
-    offset = abs(min(data['plot_date']))
+    offset = abs(min(0, min(data['plot_date'])))
     data['text_center'] = data['plot_date'] + offset
     data['text_top'] = data['text_center'] + (data['text_height'] / 2)
     data['text_bottom'] = abs(data['text_center']) - data['text_height']
@@ -52,30 +53,31 @@ def calculate_text_height(data: pd.DataFrame, years_per_line=100) -> pd.DataFram
     return data
 
 
-def place_text_vertical_timeline(data: pd.DataFrame, overlap_buffer: float|int = 100) -> pd.DataFrame|None:
+def place_text_vertical_timeline(data: pd.DataFrame, overlap_buffer: float|int) -> pd.DataFrame|None:
 
     # reshape and identify overlaps
     data = data.sort_values(by="plot_date", ascending=False)  # most recent values at the top
+    data['overlap_buffer'] = (data['n_lines'] + 1) * overlap_buffer
 
     # does a record top overlap the bottom of the next record?
-    data["overlaps_present"] = [False] + [True if data["text_top"].iloc[i] + overlap_buffer >= data["text_bottom"].iloc[i-1] else False for i in range(1, data.shape[0])]
+    data["overlaps_present"] = [False] + [True if data["text_top"].iloc[i] + data["overlap_buffer"].iloc[i] >= data["text_bottom"].iloc[i-1] else False for i in range(1, data.shape[0])]
 
     # bump everything down until there are no overlaps
     if data["overlaps_present"].any():
         overlap_data = data[data["overlaps_present"] == True]
         other_data = data[data["overlaps_present"] == False]
         for position in ["text_top", "text_center", "text_bottom"]:
-            overlap_data[position] = overlap_data[position] - overlap_buffer
+            overlap_data[position] = overlap_data[position] - overlap_data["overlap_buffer"]
         data = pd.concat([overlap_data, other_data])
-        return place_text_vertical_timeline(data)
+        return place_text_vertical_timeline(data, overlap_buffer=overlap_buffer)
     else:
-        offset = abs(min(data['plot_date']))
+        offset = abs(min(0, min(data['plot_date'])))
         for position in ["text_top", "text_center", "text_bottom"]:
             data[position] = data[position] - offset
         return data
 
 
-def make_tick_marks(data: pd.DataFrame, interval_years: int = 500) -> (list[int], list[str]):
+def make_tick_marks(data: pd.DataFrame, interval_years: int) -> (list[int], list[str]):
 
     data_min = round_nearest(data["plot_date"].min(), interval_years, direction="down")
     data_max = round_nearest(data["plot_date"].max(), interval_years, direction="up")
@@ -85,7 +87,7 @@ def make_tick_marks(data: pd.DataFrame, interval_years: int = 500) -> (list[int]
     return ticks, marks
 
 
-def timeline_vertical(data: pd.DataFrame) -> None:
+def timeline_vertical(data: pd.DataFrame, years: int) -> None:
 
     fig, ax = plt.subplots(figsize=(7, 6))
     ax.set(title="")
@@ -96,7 +98,7 @@ def timeline_vertical(data: pd.DataFrame) -> None:
     # Annotate the lines and draw lines to annotations
     for i, event in data.iterrows():
         ax.plot([0.1, 0.3], [event['plot_date'], event['text_top']], color="black")
-        text_position = event['text_center'] - 50
+        text_position = event['text_center'] - years/2
         ax.annotate(
             event['event'],
             xy=(0.31, text_position),
@@ -105,7 +107,7 @@ def timeline_vertical(data: pd.DataFrame) -> None:
         )
 
     # update tick marks on y-axis
-    ticks, marks = make_tick_marks(data)
+    ticks, marks = make_tick_marks(data, interval_years=years*5)
     plt.yticks(ticks, marks)
 
     # turn axis spines off; turn x-axis off
@@ -120,23 +122,32 @@ def timeline_horizontal(data: pd.DataFrame) -> None:
     raise NotImplementedError
 
 
-def main(
-    data_fpath: str = "/Users/Kelly/presentation_figures/figure_data/ecology_timeline.jsonl",
-    vertical=True
-) -> None:
+def main(data_fpath, years, vertical=True) -> None:
 
     # load files
     data = load_jsonl(data_fpath)
 
     # make the timeline
-    data = make_data_century(data)
-    data = calculate_text_height(data)
-    data = place_text_vertical_timeline(data)
+    data = aggregate_data(data, years=years)
+    data = calculate_text_height(data, years_per_line=years)
+    data = place_text_vertical_timeline(data, overlap_buffer=years)
 
     if vertical:
-        timeline_vertical(data)
+        timeline_vertical(data, years=years)
     else:
         timeline_horizontal(data)
 
 if __name__ == "__main__":
-    main()
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--data_fpath", required=True, help="Path to jsonl data file containing timeline data.")
+    parser.add_argument("-v", "--vertical", action="store_true", default=True, help="If specified, create a vertical timeline.")
+    parser.add_argument("-p", "--parameters", required=False, help="Additional parameters for timeline aesthetics.")
+    parser.add_argument("-y", "--years", required=False, type=int, help="Number of years to aggregate", default=100)
+
+    opts = parser.parse_args()
+    if opts.parameters:
+        raise NotImplementedError("Timeline aesthetics not yet implemented.")
+
+    main(opts.data_fpath, opts.years, opts.vertical)
